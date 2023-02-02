585fcd730bc39ac6aa4ee1dfef6b806885464c5a
Boomatisch <64149734+Boomatisch@users.noreply.github.com>
2023-02-02 18:09:13 +0100

    Add files via upload

---
+++ polspeak/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'PolSpeak - Police Megaphone by Bronson - RE-Release by Boomatisch | David'
+
+client_script "@NativeUI/NativeUI.lua"
+client_scripts {
+	'client.lua',
+	'config.lua'
+}
